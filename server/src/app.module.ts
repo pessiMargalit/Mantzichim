0c7a22b514a1c3cd2ed8a76762a7dfcff6a98a38
miriamYom <m0527669018@gmail.com>
2023-10-19 22:49:35 +0300

    start server with nest

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SlainController } from './slain/slain.controller';
+import { SlainModule } from './slain/slain.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [SlainModule],
+  controllers: [AppController, SlainController],
   providers: [AppService],
 })
 export class AppModule {}
