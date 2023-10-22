2c14b57668f990863cc5a944fa44f41895cd3213
ChaviSchatz <hand2b26@mbjcomp.org.il>
2023-10-22 12:46:57 +0300

    add text

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -30,6 +30,9 @@ export function Homepage() {
     const scrollToBottom = () => {
         ref.current?.scrollIntoView({ behavior: 'smooth' });
     }
+    let t1 = '(יוסף אומץ) "לימוד התורה מועיל שבעתיים מכל התפילה, ועל ידי כן מכניסים המת לגן עדן"'
+    let t2 = '(האר"י. ה"ע ח קח) "כי אין הקדיש בא רק להציל נפש המת מגהינם, אלא יש בו עוד תועלת גדולה להכניסו לגן עדן ולהעלותו ממדרגה למדרגה בגן עדן"'
+    // let t1 = '"לימוד התורה מועיל שבעתיים מכל התפילה שעל ידי כן מכניסים המת לגן עדן"'
     return (
         <>
             <div className="wrap-homepage">
@@ -38,9 +41,10 @@ export function Homepage() {
                     <h2 className="text">קצת הסבר על המיזם</h2>
                 </div>
                 <div className="content">
-                    <h2 className="text">אמרת חז"ל יפה על הנושא של המשניות והקדיש""</h2>
+                    <h2 className="text">{t1}</h2>
+                    <h2 className="text">{t2}</h2>
                     <div className="down">
-                        <h1>בוא וקח חלק...</h1>
+                        <h1>הצטרף ללימוד משניות ואמירת קדיש</h1>
 
                         <svg id="svg-container" onClick={scrollToBottom} xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
@@ -48,11 +52,11 @@ export function Homepage() {
                         </svg>
                     </div>
                 </div>
-                
+
                 <div ref={ref} id="my-div" className="showall">
                     <br></br><br></br><br></br><br></br><br></br><br></br>
-                    <h1>משניות לבחירה או משהו בסגנון...</h1>
-                <ShowAll></ShowAll>
+                    <h3>באפשרותך לבחור מסכת אחת או יותר או רק אמירת קדיש</h3>
+                    <ShowAll></ShowAll>
                 </div>
             </div >
         </>
