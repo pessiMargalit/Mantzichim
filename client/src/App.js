2e478e83cc59e2c206ddc3e7514a4955db30d671
ChaviDaitsh <chaviDaitsh@gmail.com>
2023-10-24 16:18:13 +0300

    Add user model without the url and style

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import { Homepage } from './Homepage';
 import { Footer } from './Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { UserModal } from './modal/UserModal';
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage></Homepage>
+      
+
+      <Router>
+          <Routes>
+            <Route path="/" element={<Homepage/>} />
+            <Route path="/userModal" element={<UserModal/>} />
+
+          </Routes>
+      </Router>
       <Footer></Footer>
+
     </div>
   );
 }
