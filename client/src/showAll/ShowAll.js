389b8f959123beb9dcfa4d5a3ad9f2944ef2c61a
pessiMargalit <pessimargalit@gmail.com>
2023-10-28 23:40:45 +0300

    Add dev:true to package-lock.json file

--- client/src/showAll/ShowAll.js
+++ client/src/showAll/ShowAll.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import Select from "react-select";
-import "../style/showAll.css";
+import "../style/ShowAll.css";
 import { Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 
